398b9f4d37990817a88805f7d6b1d49b3ee2d7f0
nhandora123 <hieunhan2000@gmail.com>
2020-12-08 18:05:16 +0700

    updateFromNhan

---
+++ WharehouseManagement/Form/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace StockManagement.Form
+{
+    public partial class MainForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void barButtonItem1_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+
+        }
+
+        private void barButtonItem2_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem11_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem12_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
--- WharehouseManagement/Program.cs
+++ WharehouseManagement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StockManagement.Form;
 
 namespace StockManagement
 {
@@ -16,7 +17,7 @@ namespace StockManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
