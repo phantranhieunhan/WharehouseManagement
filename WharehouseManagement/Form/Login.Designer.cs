529c67a673ef77d0d5be99bbdd1a288567fd03bf
nhandora123 <hieunhan2000@gmail.com>
2020-12-14 13:15:08 +0700

    abc

---
+++ WharehouseManagement/Form/Login.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace StockManagement.Form
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_Login = new DevExpress.XtraEditors.LabelControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.txt_username = new DevExpress.XtraEditors.TextEdit();
+            this.txt_password = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.bt_submit = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_username.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_password.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_Login
+            // 
+            this.lb_Login.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Login.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.lb_Login.Appearance.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Login.Appearance.Options.UseBackColor = true;
+            this.lb_Login.Appearance.Options.UseFont = true;
+            this.lb_Login.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lb_Login.IndentBetweenImageAndText = 10;
+            this.lb_Login.Location = new System.Drawing.Point(213, 134);
+            this.lb_Login.Name = "lb_Login";
+            this.lb_Login.Padding = new System.Windows.Forms.Padding(10);
+            this.lb_Login.Size = new System.Drawing.Size(369, 110);
+            this.lb_Login.TabIndex = 0;
+            this.lb_Login.Text = "Đăng nhập";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panelControl1.Appearance.BackColor2 = System.Drawing.Color.Red;
+            this.panelControl1.Appearance.Options.UseBackColor = true;
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.LookAndFeel.SkinMaskColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelControl1.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Aqua;
+            this.panelControl1.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(823, 188);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(379, 293);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Properties.Appearance.Options.UseFont = true;
+            this.txt_username.Size = new System.Drawing.Size(203, 28);
+            this.txt_username.TabIndex = 2;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(379, 354);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Properties.Appearance.Options.UseFont = true;
+            this.txt_password.Properties.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(203, 28);
+            this.txt_password.TabIndex = 2;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(213, 295);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(134, 24);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Tên đăng nhập";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(213, 356);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(82, 24);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "Mật khẩu";
+            // 
+            // bt_submit
+            // 
+            this.bt_submit.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_submit.Appearance.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.bt_submit.Appearance.Options.UseFont = true;
+            this.bt_submit.Appearance.Options.UseForeColor = true;
+            this.bt_submit.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.bt_submit.Location = new System.Drawing.Point(298, 455);
+            this.bt_submit.Name = "bt_submit";
+            this.bt_submit.Size = new System.Drawing.Size(152, 43);
+            this.bt_submit.TabIndex = 4;
+            this.bt_submit.Text = "Đăng nhập";
+            this.bt_submit.Click += new System.EventHandler(this.bt_submit_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 579);
+            this.Controls.Add(this.bt_submit);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.lb_Login);
+            this.Controls.Add(this.panelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_username.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_password.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl lb_Login;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.TextEdit txt_username;
+        private DevExpress.XtraEditors.TextEdit txt_password;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton bt_submit;
+    }
+}
