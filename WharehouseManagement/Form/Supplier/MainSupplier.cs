2c54850d9ebfd9e8501599acc50c737dbd25042a
nhandora123 <hieunhan2000@gmail.com>
2020-12-28 17:59:57 +0700

    123

---
+++ WharehouseManagement/Form/Supplier/MainSupplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace StockManagement.Form.Supplier
+{
+    public partial class MainSupplier : DevExpress.XtraEditors.XtraForm
+    {
+        public MainSupplier()
+        {
+            InitializeComponent();
+        }
+    }
+}
